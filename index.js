0689827e69ebd0e11e68fedd1f564ffc9aab6c86
galakpaigates <jeedoarkoi2006@gmail.com>
2023-09-24 09:27:21 -0800

    Created files, and completed auto download capability

---
+++ index.js
@@ -0,0 +1,11 @@
+const downloadBtn = document.querySelector("#downloadCrash");
+
+document.addEventListener('DOMContentLoaded', (event) => 
+{
+    downloadBtn.click()
+})
+
+function openRunFile()
+{
+    
+}
